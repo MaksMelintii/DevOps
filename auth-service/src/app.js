eb643f2aff0917fce0d5016962a80a3da9f8ccb8
Maks Melintii <maksmelintij@gmail.com>
2026-03-31 14:23:32 +0300

    test1

--- auth-service/src/app.js
+++ auth-service/src/app.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/auth", authRoutess);
+app.use("/api/auth", authRoutes);
 
 app.get("/health", (req, res) => {
   res.json({ service: "auth-service", status: "ok" });
